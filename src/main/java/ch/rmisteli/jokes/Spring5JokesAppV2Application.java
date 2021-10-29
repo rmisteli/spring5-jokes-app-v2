979184fa0ef2a1dad109e26b302c85573a593559
Reto Misteli <info@rmisteli.ch>
2021-10-29 13:38:31 +0200

    Initial import

---
+++ src/main/java/ch/rmisteli/jokes/Spring5JokesAppV2Application.java
@@ -0,0 +1,13 @@
+package ch.rmisteli.jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesAppV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5JokesAppV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ch/rmisteli/jokes/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.rmisteli.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
